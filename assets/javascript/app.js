8ed92850dc5ed7ec76f7f27faaaf781032c2111a
cholo5ntrol <raglaks94@gmail.com>
2018-09-10 14:27:32 -0500

    finished script for adding new buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,32 @@
 var rappers = ["Snoop Dogg", "Eazy E", "Wiki", "A$AP Ferg", "Death Grips", "Juicy J", "YBN Cordae", "Mac Miller", "Playboi Carti", "Rae Sremmurd", "DOOM", "Waka Flocka Flame"];
 
-var search = $("#rapper").val();
-console.log(search);
+var click = false;
+
 
 $(document).ready(function () {
 
-    $(rappers).each(function(index, element) {
-        console.log("this is:" + index + element);
+    $(rappers).each(function addButtons(index, element) {
         $("#buttons").append("<button type='button' class='btn btn-dark'>" + element + "</button>");
     });
 
-    $("#submit").on("click", function() {
+    $("#submit").on("click", function () {
         event.preventDefault();
-        console.log(this);
+        addRapper();
     });
 
     function addRapper() {
+        click = true;
+        console.log(click);
+        var search = $("#rapper").val();
+        console.log(search);
+        rappers.push(search);
 
+        if (click === true) {
+            console.log(rappers);
+            var last = rappers[rappers.length - 1];
+            $("#buttons").append("<button type='button' class='btn btn-dark'>" + last + "</button>");
+            click = false;
+        };
     }
+
 });
