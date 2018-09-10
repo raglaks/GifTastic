cfdab1df5c336be1fb8015a8b9f82a93d2dca30d
cholo5ntrol <raglaks94@gmail.com>
2018-09-09 22:54:00 -0500

    still working on add button script; fixed some layout quirks

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,21 @@
-var rappers = ["Snoop Dogg", "Eazy E", "Wiki", "A$AP Ferg", "Death Grips", "Juicy J", "YBN Cordae", "Mac Miller", "Playboi Carti", "Rae Sremmurd", "DOOM", "KKB", "Waka Flocka Flame"];
+var rappers = ["Snoop Dogg", "Eazy E", "Wiki", "A$AP Ferg", "Death Grips", "Juicy J", "YBN Cordae", "Mac Miller", "Playboi Carti", "Rae Sremmurd", "DOOM", "Waka Flocka Flame"];
+
+var search = $("#rapper").val();
+console.log(search);
 
 $(document).ready(function () {
+
     $(rappers).each(function(index, element) {
         console.log("this is:" + index + element);
         $("#buttons").append("<button type='button' class='btn btn-dark'>" + element + "</button>");
     });
 
+    $("#submit").on("click", function() {
+        event.preventDefault();
+        console.log(this);
+    });
+
     function addRapper() {
-        
+
     }
 });
