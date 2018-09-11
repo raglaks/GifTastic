26aa90aec9112f122ece4f4639d05cd1133c1932
cholo5ntrol <raglaks94@gmail.com>
2018-09-11 16:46:38 -0500

    finished script for loading still version of gifs and playing them when clicked; need to fix it so that gif stops when clicked again

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,18 +25,48 @@ $(document).ready(function () {
                 var results = response.data;
 
                 $(results).each(function (index) {
-                    var gifDiv = $("<span>");
+                    var gifDiv = $("<div class='text-center'>");
                     var rating = results[index].rating;
                     var p = $("<p>").text("Rating: " + rating);
+
                     var src = results[index].images.fixed_height_still.url;
+                    var animate = results[index].images.fixed_height.url;
+                    var still = results[index].images.fixed_height_still.url;
+                    var state = "still";
+
                     var gif = $("<img>");
 
                     gif.attr("src", src);
+                    gif.attr("data-state", state);
+                    gif.attr("data-still", still);
+                    gif.attr("data-animate", animate);
 
                     gifDiv.prepend(p);
                     gifDiv.prepend(gif);
 
                     $("#gifs").prepend(gifDiv);
+
+                    $(gif).on("click", function () {
+                        if (state == "animate") {
+                            $(this).attr("src", still);
+                            $(this).attr("data-state", "still");
+                            console.log(this);
+                        }
+
+                        // if (state == "still") {
+                        //     $(this).attr("src", animate);
+                        //     $(this).attr("data-state", "animate");
+                        //     console.log(this);
+                        //     console.log(state);
+                        // } else {
+                        //     $(this).attr("src", still);
+                        //     $(this).attr("data-state", "still");
+                        //     console.log(this);
+                        // }
+
+                    });
+
+                    console.log(gif);
                 });
 
             });
@@ -51,13 +81,14 @@ $(document).ready(function () {
     function addRapper() {
         click = true;
         var search = $("#rapper").val();
+        console.log(search);
         rappers.push(search);
 
         if (click === true) {
             console.log(rappers);
             var last = rappers[rappers.length - 1];
             var index = rappers.indexOf(last);
-            $("#buttons").append("<button type='button' class='button btn btn-dark' id=" + index + ">" + last + "</button>");
+            $("#buttons").append("<button type='button' class='button capitalize btn btn-dark' id=" + index + ">" + last + "</button>");
             click = false;
             buttonClick();
         };
